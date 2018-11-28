df06cfaabc6fc3cc369e492f88f215a77155ea00
b3n-j4m1n <donniemillan@gmail.com>
2018-11-28 20:49:44 +1100

    comments

--- flood-kick-sniff.sh
+++ flood-kick-sniff.sh
@@ -2,9 +2,9 @@
 
 initialise='true'
 delay='15'
-beacon_list='beacon-list'
-kill_list='kill-list'
-mac_list='mac-list'
+beacon_list='beacon-list' # beacon list taken from https://github.com/wifiphisher/wifiphisher/blob/master/wifiphisher/data/wifiphisher-known-open-wlans
+kill_list='kill-list' # mac address list of access points to deauth
+mac_list='mac-list' # sniff targeted probe requests from specifically this mac address list of clients
 ghz2='1 2 3 4 5 6 7 8 9 10 11 12 13'
 ghz5='36 38 40 42 44 46 48 50 52 54 56 58 60 62 64 100 102 104 106 108 110 112 116 132 134 136 138 140 142 144 149 151 153 155 157 159 161 165'
 channels=$ghz2
@@ -82,6 +82,7 @@ sniff() {
 	if [[ $sniff_probes = 'true' ]]
 	then
 		echo
+        	# display filters and options - https://www.wireshark.org/docs/man-pages/tshark.html
 		tshark -i $interface -I -n -Y 'wlan.fc.type_subtype == 0x0004'' and !(wlan.ssid == "")'' and wlan.tag.number == 0'"$macs" -T fields -e wlan.ta -e wlan_radio.channel -e wlan.ssid 2>/dev/null &
 	fi
 }
@@ -103,6 +104,7 @@ deauth() {
 filter() {
 	if [[ $target_macs = 'true' ]]
 	then
+        	# formatting the client mac list for the tshark command
 		macs=$(cat $mac_list | tr '\n' ' ' | sed 's/.$//' | sed 's/ / || wlan.ta == /g' | sed 's/^/ and wlan.ta == /')
 	fi
 }
@@ -113,22 +115,22 @@ cleanup() {
 }
 
 usage() {
-	echo	"usage: $0 [-afhsx] [-D interface] [-b file] [-c channel(s)] [-C channel(s)] [-i interface] [-K file] [-m mac] [-t number]"
-	echo	"	-a		enable transmitter mac address filtering"
+	echo	"usage: $0 [-afhsx] [-D interface] [-b file] [-c channel(s)] [-C channel(s)] [-i interface] [-K file] [-m file] [-t number]"
+	echo	"	-a		enable client mac address filtering"
 	echo	"	-f		enable beacon flooding"
 	echo	"	-h		enable channel hopping"
 	echo	"	-s		enable probe sniffing"
 	echo	"	-x		disable initialisation"
-	echo	"	-D <interface>	enable deauthentication on secondary interface"
-	echo	"	-b <file>	specify non-default known beacon list"
-	echo	"	-c <channel(s)>	specify primary interface channel(s), default = 2 GHz spectrum, \"5ghz\" = 5 GHz spectrum, list channels e.g. \"1 3 7\", or \"all\" = all"
-	echo	"	-C <channel(s)>	specify secondary interface channel(s)"
 	echo	"	-i <interface>	specify primary interface for flooding / hopping / sniffing"
-	echo	"	-K <file>	specify non-default deauthentication kill list"
-	echo	"	-m <file>	specify non-default mac filter list"
+	echo	"	-c <channel(s)>	specify primary interface channel(s), default = 2 GHz spectrum, \"5ghz\" = 5 GHz spectrum, \"all\" = all, or list channels e.g. \"1 3 7\""
+	echo	"	-D <interface>	enable deauthentication on specified secondary interface"
+	echo	"	-C <channel(s)>	specify secondary interface channel(s)"
+	echo	"	-b <file>	specify beacon list, default is ./beacon-list"
+	echo	"	-K <file>	specify kill list, default is ./kill-list"
+	echo	"	-m <file>	specify client list, default is ./mac-list"
 	echo	"	-r <number>	beacon flood rate per second, default = 50"
 	echo	"	-t <number>	time in seconds between channel hopping, default = 15"
-	echo	"example: $0 -afhs -b /opt/dict/beacon-list -c \"1 3 5 7 9 11\" -i wlan0 -m aa:aa:aa:aa:aa:aa -t 30 -r 25 -D wlan1 -C \"1 6 11\" -K /tmp/kill-list"
+	echo	"example: $0 -afhs -b /tmp/beacon.list -c \"1 3 5 7 9 11\" -i wlan0 -m /tmp/client.list -t 30 -r 25 -D wlan1 -C \"1 6 11\" -K /tmp/kill.list"
 	exit 1
 }
 
